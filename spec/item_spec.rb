3fdf69c4f5ade916eac3d5ce78580f184bb8842d
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-26 09:12:05 -0600

    Create item object and tests

---
+++ spec/item_spec.rb
@@ -0,0 +1,15 @@
+require './spec/spec_helper'
+
+RSpec.describe Item do
+  before(:each) do
+    @item1 = Item.new('Chalkware Piggy Bank')
+    @item2 = Item.new('Bamboo Picture Frame')
+  end
+  describe '#initialize' do
+    it 'can initialize' do
+      expect(@item1).to be_a(Item)
+      expect(@item2).to be_a(Item)
+      expect(@item1.name).to eq("Chalkware Piggy Bank")
+    end
+  end
+end
